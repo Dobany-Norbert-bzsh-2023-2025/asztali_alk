bb52f3205b0f50a03a1bfc78698691a1c687dfc9
DN <ndobany@gmail.com>
2024-04-11 08:19:33 +0200

    2024-04-10 WPF

---
+++ 2024-04-10/WPFLogin/WPFLogin/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            System.Threading.Thread.Sleep(10000);
+            string[] args = System.Environment.GetCommandLineArgs();
+            /*
+            if (args != null && args.Length > 0)
+            {
+                MessageBox.Show("Argumentumok: " + String.Join(", ", args));
+            }
+            else
+            {
+                MessageBox.Show("Nincsenek argumentumok");
+            }*/
+            if(args.Length==3)
+            {
+                tbUsername.Text = args[1];
+                tbPassword.Text = args[2];
+            }
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            IsolatedStorageFile isf = IsolatedStorageFile.GetMachineStoreForApplication();
+            IsolatedStorageFileStream fs = new IsolatedStorageFileStream("technikum.txt", System.IO.FileMode.Append, isf);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(tbUsername.Text+"; "+tbPassword.Text+"; "+DateTime.Now);
+
+            sw.Close();
+            fs.Close();
+            isf.Close();
+
+            MessageBox.Show("Sikeres bejelentkezés!");
+        }
+
+
+        private void tbUsername_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbUsername.Text.Equals("Username")) tbUsername.Text = "";
+        }
+        private void tbUsername_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbUsername.Text.Equals("")) tbUsername.Text = "Username";
+        }
+        private void tbPassword_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbUsername.Text.Equals("Password")) tbUsername.Text = "";
+        }
+        private void tbPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tbUsername.Text.Equals("")) tbUsername.Text = "Password";
+        }
+
+        
+    }
+}
---
+++ 2024-04-10/WPFReflex/WPFReflex/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFReflex
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public double width = 800;
+        public double height = 450;
+
+        public int Kattszam = 0;
+        public double OsszesIdo = 0;
+        public DateTime Most;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Canvas.SetLeft(bird, 350);
+            Canvas.SetTop(bird, 175);
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if (Kattszam == 0)
+            {
+                Most = DateTime.Now;
+            }
+            else
+            {
+                OsszesIdo += (DateTime.Now - Most).TotalMilliseconds;
+                Most=DateTime.Now;
+            }
+
+            Random rnd = new Random();
+            Canvas.SetLeft(bird,rnd.Next(0,(int)width-100));
+            Canvas.SetTop(bird, rnd.Next(0,(int)height-100));
+
+            Kattszam++;
+            if (Kattszam == 11)
+            {
+                MessageBox.Show(OsszesIdo/10+"msec az átlagod");
+                Kattszam = 0;
+                OsszesIdo = 0;
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //MessageBox.Show("méret");
+            width = e.NewSize.Width;
+            height = e.NewSize.Height;
+            lblMessage.Width = width;
+
+            double mostLeft = Canvas.GetLeft(bird);
+            if (mostLeft>width-130)
+            {
+                Canvas.SetLeft(bird, width - 130);
+            }
+
+            double mostTop = Canvas.GetTop(bird);
+            if(mostTop>height-130)
+            {
+                Canvas.SetTop(bird, height - 130);
+            }
+        }
+
+        
+    }
+}
